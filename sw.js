2cfc3576e4108b17cf9cc01baeffdcc5f8915840
Project Georgia <projectgeorgia0.0@gmail.com>
2025-03-08 17:54:49 +0800

    .

--- sw.js
+++ sw.js
@@ -12,6 +12,7 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(ASSETS))
+    );
 });
 
 self.addEventListener('fetch', event => {
