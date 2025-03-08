47ce8217cd09f6c6fca95b1ff1913cf01714ab7d
Project Georgia <projectgeorgia0.0@gmail.com>
2025-03-08 18:13:22 +0800

    .

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'electricity-pwa-v2';
+const CACHE_NAME = 'electricity-pwa-v3';
 const ASSETS = [
     '/',
     '/index.html',
@@ -6,19 +6,16 @@ const ASSETS = [
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
     'https://fonts.googleapis.com/css2?family=Dosis:wght@400;500;600&display=swap',
-    // Add all payment logo URLs here
-    'https://www.hkma.gov.hk/media/eng/doc/key-functions/financial-infrastructure/retail-payment/fps/logo/fps_logo_2017_eng.png',
-    'https://www.octopus.com.hk/img/logo.svg',
-    'https://www.payme.hsbc.com.hk/images/payme_logo.svg',
-    'https://www.apple.com/apple-pay/images/overview/icon_applepay.png',
-    'https://developer.samsung.com/one/samsungpay/images/samsung_pay_logo_blue_rgb.png',
-    'https://pay.google.com/about/static/images/social/og_image.jpg',
-    'https://www.alipay.com/h5app/static/image/logo.png',
-    'https://pay.weixin.qq.com/images/wechat_pay/wechatpay_logo_icon.png',
-    'https://www.paypalobjects.com/webstatic/mktg/logo/pp_cc_mark_37x23.jpg',
-    'https://www.visa.com.hk/dam/VCOM/regional/ap/hongkong/global-elements/images/visa-logo-800x450.jpg',
-    'https://brand.mastercard.com/content/dam/mccom/brandcenter/thumbnails/mastercard_circles_92px_2x.png',
-    'https://www.unionpayintl.com/en/img/logo2x.png'
+    // Payment logos
+    'https://upload.wikimedia.org/wikipedia/commons/6/6d/Faster_Payment_System_%28HK%29_logo.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/5/59/Octopus_card.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/7/7c/PayMe_by_HSBC_Logo.png',
+    'https://upload.wikimedia.org/wikipedia/commons/f/fd/Alipay.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/a/ae/WeChat_Pay_Logo.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/3/39/PayPal_logo.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/5/5e/Visa_Inc._logo.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/2/2a/Mastercard-logo.svg',
+    'https://upload.wikimedia.org/wikipedia/commons/3/33/UnionPay_logo.svg'
 ];
 
 self.addEventListener('install', event => {
