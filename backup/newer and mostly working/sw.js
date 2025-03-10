34638c308f88ce39f143796ce2b62a8283b8010d
Project Georgia <projectgeorgia0.0@gmail.com>
2025-03-10 12:34:57 +0800

    .

--- backup/newer and mostly working/sw.js
+++ backup/newer and mostly working/sw.js
@@ -3,6 +3,7 @@ const ASSETS = [
     '/',
     '/index.html',
     '/manifest.json',
+    '/logo-1024.svg',
     '/icon-192.png',
     '/icon-512.png',
     'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
