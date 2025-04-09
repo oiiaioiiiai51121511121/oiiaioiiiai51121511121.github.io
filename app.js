4b3c44cf070ff88a1f70cf9f6292e0501d73701c
Project Georgia <projectgeorgia0.0@gmail.com>
2025-04-09 16:04:04 +0800

    .

---
+++ app.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAzoj-EGSyjhctXm7H6VhZQkPXpfx1MwE4",
+    authDomain: "project-5399799771103896588.firebaseapp.com",
+    projectId: "project-5399799771103896588",
+    messagingSenderId: "159455371624",
+    appId: "1:159455371624:web:a9df800233b6c04da2f6dd",
+    measurementId: "G-LCD6Q3BE52"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
