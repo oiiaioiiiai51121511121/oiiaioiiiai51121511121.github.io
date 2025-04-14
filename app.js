f8f824492f63ea23f8d8cf239b9577c140650c67
Project Georgia <projectgeorgia0.0@gmail.com>
2025-04-14 17:50:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAzoj-EGSyjhctXm7H6VhZQkPXpfx1MwE4",
-    authDomain: "project-5399799771103896588.firebaseapp.com",
-    databaseURL: "https://project-5399799771103896588-default-rtdb.firebaseio.com",
-    projectId: "project-5399799771103896588",
-    storageBucket: "project-5399799771103896588.firebasestorage.app",
-    messagingSenderId: "159455371624",
-    appId: "1:159455371624:web:a9df800233b6c04da2f6dd",
-    measurementId: "G-LCD6Q3BE52"
-};
+    apiKey: "AIzaSyAYkRIeFVnvqQ-epYMM2HeZ2yaXFvcZR9Q",
+    authDomain: "ee3070-c2c77.firebaseapp.com",
+    databaseURL: "https://ee3070-c2c77-default-rtdb.firebaseio.com",
+    projectId: "ee3070-c2c77",
+    storageBucket: "ee3070-c2c77.firebasestorage.app",
+    messagingSenderId: "578057381631",
+    appId: "1:578057381631:web:301014aebf389e4c264eca",
+    measurementId: "G-TPE9ZH17VG"
+  };
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
